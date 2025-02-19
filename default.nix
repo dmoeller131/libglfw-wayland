5af086cd471e918bf1b0dddc78314c75867f4673
-Daniel- <99113259+dmoeller131@users.noreply.github.com>
2025-02-18 20:35:03 -0600

    Update default.nix

--- default.nix
+++ default.nix
@@ -2,17 +2,17 @@
 
 stdenv.mkDerivation rec {
   pname = "libglfw-wayland";
-  version = "3.5";
+  # version = "3.5";
 
 
   src = fetchurl {
-    name = "libglfw.so.${version}";
-    url = "https://github.com/BoyOrigin/glfw-wayland/releases/download/2024-03-07/libglfw.so.${version}";
+    # name = "libglfw.so.${version}";
+    url = "https://github.com/dmoeller131/libglfw-wayland/archive/refs/tags/v4.tar.gz";
     sha256 = "1gdd0hhlkr7if9pinalr9alqpibiik501afynid68x8q633bsnb5"; # Replace with the actual sha256 hash of the file
   };
 
   nativeBuildInputs = [];
-  phases = [ "installPhase" ]; # Removes all phases except installPhase
+  # phases = [ "installPhase" ]; # Removes all phases except installPhase
 
   installPhase = ''
     mkdir -p $out/lib
