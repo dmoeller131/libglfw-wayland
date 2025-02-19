dd7f2964cc6a7fa5dd62332ba4aa0a9582dd3f57
dmoeller131 <danielmoeller01@gmail.com>
2025-02-18 19:55:06 -0600

    a

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     name = "libglfw.so.${version}";
     url = "https://github.com/BoyOrigin/glfw-wayland/releases/download/2024-03-07/libglfw.so.${version}";
-    sha256 = "sha256-hash-of-the-file"; # Replace with the actual sha256 hash of the file
+    sha256 = "1gdd0hhlkr7if9pinalr9alqpibiik501afynid68x8q633bsnb5"; # Replace with the actual sha256 hash of the file
   };
 
   nativeBuildInputs = [];
